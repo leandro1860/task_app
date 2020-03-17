22f92b7f89786f5265475f33217ff7450edcaf47
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-03-17 09:14:11 -0300

    Updates

---
+++ precache-manifest.f68c8cf6c6ab211a250e284984959df6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f27459476f9955d81d3ddfa69496b2",
+    "url": "/task_app/index.html"
+  },
+  {
+    "revision": "a6ee3495ce869b98d12b",
+    "url": "/task_app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a89c367c04af2b5c159f",
+    "url": "/task_app/static/css/main.d80d4bf5.chunk.css"
+  },
+  {
+    "revision": "a6ee3495ce869b98d12b",
+    "url": "/task_app/static/js/2.41df74bc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/task_app/static/js/2.41df74bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89c367c04af2b5c159f",
+    "url": "/task_app/static/js/main.59727852.chunk.js"
+  },
+  {
+    "revision": "0d6947fcf19cb5903422",
+    "url": "/task_app/static/js/runtime-main.38c64987.js"
+  }
+]);
